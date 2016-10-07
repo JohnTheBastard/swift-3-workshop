ccf1fcbfdc48d8e6deb12efb0babfa7a5a97f5c4
John Hearn <jhearn@gmail.com>
2016-10-07 11:03:01 -0700

    initial commit

---
+++ CFS3ToDoList/models/Identity.swift
@@ -0,0 +1,9 @@
+//
+//  Identity.swift
+//  CFS3ToDoList
+//
+//  Created by John D Hearn on 10/7/16.
+//  Copyright © 2016 Adam Wallraff. All rights reserved.
+//
+
+import Foundation
---
+++ CFS3ToDoList/models/Todo.swift
@@ -0,0 +1,21 @@
+//
+//  Todo.swift
+//  CFS3ToDoList
+//
+//  Created by John D Hearn on 10/7/16.
+//  Copyright © 2016 Adam Wallraff. All rights reserved.
+//
+
+import Foundation
+
+class Todo{
+    var text: String
+    var identifier: String
+
+    init(text: String) {
+        self.text = text
+        self.identifier = UUID().uuidString
+    }
+}
+
+
